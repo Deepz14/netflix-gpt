c1ca2aa37f162ff102dc18ded147e3ef44ae7f73
deepak <deepak.c@telliant.net>
2023-11-05 13:36:29 +0530

    browse navbar component added

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,33 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { remove } from "../store/userSlice";
+import Navbar from "./Navbar";
 
 const Browse = () => {
-    const userInfo = useSelector((state) => state.user);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const handlerLogout = () => {
-        dispatch(remove());
-        navigate("/");
-    }
-
     return (
         <div>
-            <nav className="w-full shadow flex items-center justify-between py-3 px-3">
-                <h1>Netflix</h1>
-                <ul className="flex items-center">
-                    <li>{userInfo?.displayName}</li>
-                    <li>
-                        <button onClick={handlerLogout}
-                            className="px-5 py-2 mx-3 rounded hover:bg-red-500 hover:text-white">
-                                Logout
-                        </button>
-                    </li>
-                </ul>
-            </nav>
- 
+          <Navbar />
         </div>
     )
 }
